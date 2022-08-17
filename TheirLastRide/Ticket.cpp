2be34528c74099191ef52964054a780362d16e8d
bailuob0 <tanialam1101@gmail.com>
2022-08-17 10:53:42 +0800

    added ticket class

--- TheirLastRide/Ticket.cpp
+++ TheirLastRide/Ticket.cpp
@@ -1,1 +1,16 @@
 #include "Ticket.h"
+
+std::string Ticket::getIssuingStn()
+{
+    return _IssuingStn;
+}
+
+std::string Ticket::getDestination()
+{
+    return _Destination;
+}
+
+std::string Ticket::getIssueDate()
+{
+    return _IssueDate;
+}
