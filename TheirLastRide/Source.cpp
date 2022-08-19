a07207b3f8f967035e9de39b912ecbcaa7156dcd
JustACoderWanderingAround <pgbaquiran@gmail.com>
2022-08-19 17:30:53 +0800

    added ticket test (uncomment to run)

--- TheirLastRide/Source.cpp
+++ TheirLastRide/Source.cpp
@@ -12,5 +12,18 @@ int main(void)
 	app ->Init();
 	app ->Run();
 	app ->Exit();
+	/*Ticket* ticket1 = new Ticket("Stop A", "Stop B", 1);
+	RailPass* railpass1 = new RailPass("name", false, 1);
+	Player* playerOne = new Player({ "Stop A", "Stop B" });
+
+	InteractablePerson* pass1 = new InteractablePerson("name", false, false, ticket1, railpass1);
+
+	std::cout << playerOne->compareToBook(*pass1, 1) << std::endl;
+	std::cout << playerOne->compareToBook(*pass1, 2) << std::endl;
+	std::cout << playerOne->compareToBook(*pass1, 3) << std::endl;
+	std::cout << playerOne->compareToBook(*pass1, 4) << std::endl;
+	std::cout << playerOne->compareToBook(*pass1, 5) << std::endl;
+	std::cout << playerOne->compareToBook(*pass1, 6) << std::endl;*/
+
 	return 0;
 }
