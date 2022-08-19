5de61beeb90056add570a354ad21c218224df765
Christian <97773844+esuriru@users.noreply.github.com>
2022-08-20 00:03:27 +0800

    Small typo

--- TheirLastRide/Node.h
+++ TheirLastRide/Node.h
@@ -9,7 +9,7 @@ using json = nlohmann::json;
 /// </summary>
 /// <param name="playerText">The text that the player will say to the NPC (usually will be shown as an option in a button)</param>
 /// <param name="npcText">The text that the NPC will say to the player (could be a question or something)</param>
-/// <param name="npcText">(Nodes are in sequence) The list of where the node could potentially branch towards based on the option chosen to implement flow and logic (Remember that the first node is 0.)</param>
+/// <param name="results">(Nodes are in sequence) The list of where the node could potentially branch towards based on the option chosen to implement flow and logic (Remember that the first node is 0.)</param>
 struct Node
 {
 	//data
