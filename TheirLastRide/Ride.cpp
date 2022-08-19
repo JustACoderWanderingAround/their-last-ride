429e30771272b007d15d47452af553d5746339ca
JustACoderWanderingAround <pgbaquiran@gmail.com>
2022-08-19 14:43:07 +0800

    added function for parsing json, currently broken, need fix.

--- TheirLastRide/Ride.cpp
+++ TheirLastRide/Ride.cpp
@@ -4,9 +4,19 @@
 #include "json.hpp"
 using json = nlohmann::json;
 
+void to_json(json& j, const Ride& n) {
+	j = json{ {"start", n.start}, {"stops", n.stops }};
+}
+
+void from_json(const json& j,Ride& n) {
+	j.at("start").get_to(n.start);
+	j.at("stops").get_to(n.stops);
+}
+
 Ride::Ride()
 
 {
+	loadAttributes();
 }
 
 Ride::Ride(std::string str, std::vector<std::string> stp, int iN, int nN)
@@ -36,7 +46,7 @@ int Ride::getNonInteractable()
 bool Ride::loadAttributes()
 {
 	//TODO: make this into a loadJson function
-	std::ifstream f("Data\\Rides.json");
+	std::ifstream f("Data\\Ride.json");
 	json j;
 	if (!f) {
 		std::cout << "File not loaded succesfully.\n";
@@ -45,7 +55,7 @@ bool Ride::loadAttributes()
 	else {
 		j = json::parse(f);
 	}
-	
+	//std::vector<Ride> testRide = j.get<std::vector<Ride>>();
 	//std::cout << _nodes[0];
 	return true;
 	/*for (int i = 0; i < tempNodes2.size(); i++)
