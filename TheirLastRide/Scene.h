444fff719f20da83dd72b3ea981204ee923a9253
JustACoderWanderingAround <pgbaquiran@gmail.com>
2022-08-20 16:14:44 +0800

    added player and ride attributes to scene

--- TheirLastRide/Scene.h
+++ TheirLastRide/Scene.h
@@ -2,14 +2,17 @@
 #define SDL_MAIN_HANDLED
 #include <SDL.h>
 #include "Application.h"
+#include "Player.h"
+#include "Ride.h"
 /// <summary>
 /// Scenes are to be derived classes of this abstract class.
 /// </summary>
 class Scene
 {
+Player* mainPlayer;
+Ride* mainRide;
 public:
 	Scene();
-
 	virtual void Init() = 0;
 	virtual void Update(double dt) = 0;
 	virtual void Render() = 0;
