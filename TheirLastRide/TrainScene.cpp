2a6740297f542e33b566df1014a3dfa3fd4268f2
bailuob0 <tanialam1101@gmail.com>
2022-08-23 09:26:53 +0800

    fixed punch and render order

--- TheirLastRide/TrainScene.cpp
+++ TheirLastRide/TrainScene.cpp
@@ -271,13 +271,18 @@ void TrainScene::Render()
     renderCabins();
     // UI Rendering
     if (isInteracting) {
+        
         RenderAtCoords(_objList[OBJECT_TEXTBOX]);
         RenderAtCoords(_objList[OBJECT_TEXT]);
-        RenderAtCoords(_objList[OBJECT_TICKET]);
-        RenderAtCoords(_objList[OBJECT_TICKET_FROM]);
-        RenderAtCoords(_objList[OBJECT_TICKET_TO]);
-        RenderAtCoords(_objList[OBJECT_TICKET_DOI]);
 
+        if (!ticketFront)
+        {
+            RenderAtCoords(_objList[OBJECT_TICKET]);
+            RenderAtCoords(_objList[OBJECT_TICKET_FROM]);
+            RenderAtCoords(_objList[OBJECT_TICKET_TO]);
+            RenderAtCoords(_objList[OBJECT_TICKET_DOI]);
+        }
+       
         bool isRailpass = _interactingPerson; //attach to getRailpass
         if (isRailpass)
         {
@@ -526,7 +531,7 @@ void TrainScene::HandleInput()
             }
         }
 
-        if (_currentCabin + 1 < _cabins.size() && playerX >= 1000)
+        if (_currentCabin + 1 < _cabins.size() && playerX >= 1050)
         {
             _currentCabin += 1;
             playerX = 30;
